aa7fb37ac8df08e4405f28c2d153f6c9b6c29545
mrLOEPSOEP <tm.lansbergen@outlook.com>
2025-11-20 23:04:10 +0100

    Color Malfunction

---
+++ Assets/Scripts/mainMenuUIManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class mainMenuUIManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Main menu hi");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
