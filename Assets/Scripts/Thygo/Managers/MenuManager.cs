276a7ffdb0f31b65fac480f1c10a2eddf5ea8216
mrLOEPSOEP <tm.lansbergen@outlook.com>
2025-11-27 14:00:39 +0100

    LGSelection

--- Assets/Scripts/Thygo/Managers/MenuManager.cs
+++ Assets/Scripts/Thygo/Managers/MenuManager.cs
@@ -12,44 +12,65 @@ public class MenuManager : MonoBehaviour, IAmObserver<ButtonType>
     //Variables
     bool languageIsDutch;
     bool languageIsEnglish;
-    GameObject guidanceButton;
-    GameObject practiceButton;
-    GameObject advancedButton;
-    TMP_Text guidanceText;
-    TMP_Text practiceText;    
-    TMP_Text advancedText;    
+
+    //references
+    [Header("Canvas' with buttons")]
+    [SerializeField] GameObject gamemodeSelectCanvas;
+    [SerializeField] GameObject languageSelectCanvas;
+    [Header("Textfields of the buttons")]
+    [SerializeField] TMP_Text guidanceText;
+    [SerializeField] TMP_Text practiceText;    
+    [SerializeField] TMP_Text challengeText;    
     
 
-    void Awake()
-    {
-        guidanceButton = GameObject.Find("GuidanceText");
-        guidanceText = guidanceButton.GetComponent<TMP_Text>();
-        
-        practiceButton = GameObject.Find("GuidanceText");
-        practiceText = practiceButton.GetComponent<TMP_Text>();
-
-        advancedButton = GameObject.Find("GuidanceText");
-        advancedText = advancedButton.GetComponent<TMP_Text>();
-    }
     
     public void OnNotify(ButtonType value)
     {
         Debug.Log("Recieved message " + value);
         if (value == ButtonType.English)
         {
+            languageSelectCanvas.SetActive(false);
+            gamemodeSelectCanvas.SetActive(true);
             languageIsEnglish = true;
         }
 
         if (value == ButtonType.Dutch)
         {
+            languageSelectCanvas.SetActive(false);
+            gamemodeSelectCanvas.SetActive(true);
             languageIsDutch = true;
-            guidanceText.text = "languageIsDutch";
         }   
+        //GetButtons();
+        SetLanguage();
     }
 
+    /*/void GetButtons()
+    {
+        //finding the text areas of the gamemode select screen
+        guidanceButton = GameObject.Find("GuidanceText");
+        guidanceText = guidanceButton.GetComponent<TMP_Text>();
+        
+        practiceButton = GameObject.Find("PracticeText");
+        practiceText = practiceButton.GetComponent<TMP_Text>();
+
+        challengeButton = GameObject.Find("ChallengeText");
+        challengeText = challengeButton.GetComponent<TMP_Text>();
+    }*/
     void SetLanguage()
     {
         //if (languageIsDutch);
+        if (languageIsDutch)
+        {
+            guidanceText.text = "Hulpmodus";
+            practiceText.text = "Oefenmodus";
+            challengeText.text = "Uitdagingsmodus";
+        }
+        else if (languageIsEnglish)
+        {
+            guidanceText.text = "Guidance mode";
+            practiceText.text = "Practice mode";
+            challengeText.text = "Challange mode";
+        }
     }
 
     
