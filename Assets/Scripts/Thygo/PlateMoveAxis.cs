f216a30608c7ff121b1ec090b7f968c0ca9eab51
mrLOEPSOEP <tm.lansbergen@outlook.com>
2025-12-05 15:14:08 +0100

    Update PlateMoveAxis.cs

--- Assets/Scripts/Thygo/PlateMoveAxis.cs
+++ Assets/Scripts/Thygo/PlateMoveAxis.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlateMoveAxis : MonoBehaviour, IAmObserver<AxisData>
 {
     [SerializeField] XRSubject<AxisData> crank;
+    [SerializeField] GameObject plate;
     Vector3 platePosition;
+
+    [SerializeField] bool moveTypeX;
+    [SerializeField] bool moveTypeY;
+    [SerializeField] bool moveTypeZ;
 
     void Start()
     {
