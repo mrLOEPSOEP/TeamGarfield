270d0d783fa89e00c088a8085fc2041558c9e41e
itskelvinay <itskelvinay@gmail.com>
2025-12-08 12:48:07 +0100

    More greybox
    -added more greyboxing
    - tried scriptable objects

---
+++ Assets/Scripts/Kelvin/MaterialType.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MaterialType", menuName = "Scriptable Objects/MaterialType")]
+public class MaterialType : ScriptableObject
+{
+    public string materialName;
+    public int maxSpeed;
+    
+}
---
+++ Assets/test.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    [SerializeField] private MaterialType materialType;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log(materialType.MaterialName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
