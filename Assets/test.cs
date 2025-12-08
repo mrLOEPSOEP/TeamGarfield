718636708c09ec76ec9600340f1750509c521126
mrLOEPSOEP <tm.lansbergen@outlook.com>
2025-12-08 12:51:00 +0100

    fixed compiler errors

--- Assets/test.cs
+++ Assets/test.cs
@@ -6,7 +6,7 @@ public class test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log(materialType.MaterialName);
+        Debug.Log(materialType.materialName);
     }
 
     // Update is called once per frame
