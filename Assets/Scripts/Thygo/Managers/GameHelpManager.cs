53f5e7d116eb95fe0cd6b022a5271667523cd102
mrLOEPSOEP <tm.lansbergen@outlook.com>
2025-12-11 14:26:50 +0100

    ghm

--- Assets/Scripts/Thygo/Managers/GameHelpManager.cs
+++ Assets/Scripts/Thygo/Managers/GameHelpManager.cs
@@ -2,5 +2,15 @@ using UnityEngine;
 
 public class GameHelpManager : Subject<HelpAmount>, IAmObserver<ButtonType>
 {
-    
+    //change names to what they actually are
+    //references to the thing that provides some form of help
+    [SerializeField] GameObject HelpField1;
+    [SerializeField] GameObject HelpField2;
+    [SerializeField] GameObject HelpField3;
+    [SerializeField] GameObject HelpField4;
+    [SerializeField] GameObject HelpField5;
+    [SerializeField] GameObject HelpField6;
+    [SerializeField] GameObject HelpField7;
+    [SerializeField] GameObject HelpField8;
+    [SerializeField] GameObject HelpField9;
 }
