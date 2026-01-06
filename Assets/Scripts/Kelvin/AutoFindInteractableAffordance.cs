050f2f9b8ece7d977dba0baccbcaa8ab475f02f0
c0zmic <itskelvinay@gmail.com>
2026-01-06 20:24:31 +0100

    Added test sound effects

---
+++ Assets/Scripts/Kelvin/AutoFindInteractableAffordance.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.AffordanceSystem.State;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class AutoFindInteractableAffordance : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<XRInteractableAffordanceStateProvider>().interactableSource = GetComponentInParent<XRBaseInteractable>();
+    }
+}
