a965c48fb8e74652be3320c7a50165f51c907e85
c0zmic <itskelvinay@gmail.com>
2026-01-08 11:38:05 +0100

    Added correct sound to material

--- Assets/Scripts/Thygo/ObserverPattern/MaterialSocketSubject.cs
+++ Assets/Scripts/Thygo/ObserverPattern/MaterialSocketSubject.cs
@@ -3,6 +3,11 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class MaterialSocketSubject : XRSubject<MaterialTypeData>
 {
+
+    [SerializeField] private AudioSource audioSourceClamp;
+    [SerializeField] private AudioClip audioCopper;
+    [SerializeField] private AudioClip audioSteel;
+
     //Call this throught the XRSocketInteractor's Select Entered event
     public void OnItemEntered(SelectEnterEventArgs args)
     {
@@ -12,7 +17,18 @@ public class MaterialSocketSubject : XRSubject<MaterialTypeData>
         if (material != null)
         {
             NotifyObservers(this, new MaterialTypeData{type = material.materialType, isPresent = true});
-            Debug.Log(material.materialType);
+
+            if (material.materialType.materialName == "Copper")
+            {
+                audioSourceClamp.PlayOneShot(audioCopper);
+                Debug.Log(material.materialType + " is suppose to be copper");
+            }
+            else if (material.materialType.materialName == "Steel")
+            {
+                audioSourceClamp.PlayOneShot(audioSteel);
+                Debug.Log(material.materialType + " is suppose to be steel");
+            }
+
         }
     }
 
