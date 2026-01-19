367ceea8633de7d6731f12310eef828613ca6b03
mrLOEPSOEP <tm.lansbergen@outlook.com>
2026-01-19 20:56:07 +0100

    Update DrillBitIdentifier.cs

--- Assets/Scripts/Thygo/DrillBitIdentifier.cs
+++ Assets/Scripts/Thygo/DrillBitIdentifier.cs
@@ -3,5 +3,7 @@ using UnityEngine;
 //Goes on the drillbit 
 public class DrillBitIdentifier : MonoBehaviour
 {
-    [Header("Size in mm")] public float bitSize;
+    [Header("Size in mm")] 
+    public float bitSize;
+    public Transform drillBitTip;
 }
