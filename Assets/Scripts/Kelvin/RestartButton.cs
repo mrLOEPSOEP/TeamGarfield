bc53242d1116be5c02687949e597d0b1206a5f7a
mrLOEPSOEP <tm.lansbergen@outlook.com>
2026-01-19 21:27:44 +0100

    Update RestartButton.cs

--- Assets/Scripts/Kelvin/RestartButton.cs
+++ Assets/Scripts/Kelvin/RestartButton.cs
@@ -5,6 +5,10 @@ public class RestartButton : MonoBehaviour
 {
     public void RestartGame(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        // Get the name of the scene that is currently open
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        // Load it again
+        SceneManager.LoadScene(currentSceneName);
     }
 }
